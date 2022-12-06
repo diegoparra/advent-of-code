f232d524e65afe23e7020f5326585241fd88ca60
Diego Parra <dp@linkfire.com>
2022-12-06 09:50:47 -0400

    feat: add new func

--- day-1/main.go
+++ day-1/main.go
@@ -4,12 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
-func main() {
+func sumElfs(filename string) []int {
 
-	readFile, err := os.Open("file.txt")
+	readFile, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -41,14 +42,43 @@ func main() {
 		}
 		result = append(result, count)
 	}
+	return result
+}
+
+func getBiggest(values []int) int {
 
 	var largerNumber, temp int
 
-	for _, element := range result {
+	for _, element := range values {
 		if element > temp {
 			temp = element
 			largerNumber = temp
 		}
 	}
-	fmt.Println("Largest number of Array/slice is  ", largerNumber)
+	return largerNumber
+}
+
+func getThreeBiggest(values []int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(values)))
+	var result int
+
+	for i := 0; i < 3; i++ {
+		result += values[i]
+	}
+
+	return result
+
+}
+
+func main() {
+
+	elfsConsumed := sumElfs("file.txt")
+
+	result := getBiggest(elfsConsumed)
+
+	resultThreeElfs := getThreeBiggest(elfsConsumed)
+
+	fmt.Println("Elf carring the biggest value: ", result)
+
+	fmt.Println("Total 3 Elf's: ", resultThreeElfs)
 }
